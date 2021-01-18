82ba6b542d274cf21769fd57ab0d398dab32e694
saoirse-defi <saoirsef@yahoo.co.uk>
2021-01-18 14:36:57 +0000

    brainstorm possible object properties

--- js/sprites.js
+++ js/sprites.js
@@ -4,7 +4,20 @@
 let player = {
 
     init: (game) => {
+
         this.game = game;
+
+        this.radius =    //dont know
+
+        this.xPosition = 0;
+
+        this.yPosition = 0;
+
+        this.speed = 7;
+
+        this.xVelocity = 0;
+
+        this.yVelocity = 0;
     }
 
     //draw:
@@ -15,4 +28,51 @@ let player = {
 
 let enemy = {
 
+    //init
+    init: (game) => {
+        
+        this.game = game; //possibly not needed for PIXIjs
+
+        this.reset();
+
+        this.enemyPool = [];
+
+        this.radius =  // dont know what value to use
+        this.dead = false;
+
+        this.xPosition = 0;
+        this.yPosition = 0;
+
+        this.speed = 6;
+        this.xVelocity = 0;
+        this.yVelocity = 0;
+
+        this.points = 100;
+    }
+
+    //hone
+
+    //updatePosition
+
+    //draw
+
+    //spawn
+
+    //die
+
 };
+
+let gate = {
+
+    init: (game) => {
+        
+        this.game = game;
+
+        this.reset();
+
+        this.gatePool = [];
+    }
+
+    
+
+}
