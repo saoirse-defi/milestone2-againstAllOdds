c52b6195a264a75bd52d09bfff32ed97e0983ce8
saoirse-defi <saoirsef@yahoo.co.uk>
2021-01-18 14:36:08 +0000

    add collision detection function

--- js/physics.js
+++ js/physics.js
@@ -1,1 +1,10 @@
 
+let collisionDetection = (a, b) => {
+    let hitboxA = a.getBounds();
+    let hitboxB = b.getBounds();
+
+    return hitboxA.x + hitboxA.width > hitboxB.x &&
+            hitboxA.x < hitboxB.x + hitboxB.width &&
+            hitboxA.y + hitboxA.height > hitboxB.y &&
+            hitboxA.y <hitboxB.y + hitboxB.height;
+}
