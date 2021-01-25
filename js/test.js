55d5a56da06827e3d55bd6d07a26f54655beb390
saoirse-defi <saoirsef@yahoo.co.uk>
2021-01-25 13:35:32 +0000

    add rotating gate sprite to stage

--- js/test.js
+++ js/test.js
@@ -1,12 +1,14 @@
+
 const WINDOW_WIDTH = $(window).innerHeight();
 const WINDOW_HEIGHT = $(window). innerWidth();
 
-let app, surface, texture, theGame, hero, alien1, alien2, alien3, alien4, alien5, alien6;
+let app, surface, texture, theGame, hero, gate, alien1, alien2, alien3, alien4, alien5, alien6;
 let container = {};
 let keyTracker = {};
 let keys = {};
 let enemy = {};
 let aliens = [];
+let gates = [];
 
 
 class Human extends PIXI.Sprite{
@@ -30,6 +32,28 @@ class Human extends PIXI.Sprite{
     }
 }
 
+class Gate extends PIXI.Sprite{
+    constructor(x, y, width, height, texture, speed, reward, radius){
+        super(texture);
+        this.anchor.set(0.5);
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+        this.reward = reward;
+        this.radius = radius;
+        this.xVel = 0;
+        this.yVel = 0;
+        this.rotation = 0;
+        this.rotationSpeed = 0.6;
+    }
+
+    stayWithinArea(){
+        this.x = Math.max(0 + this.radius, Math.min(this.x + this.xVel, 1000 - this.radius));
+        this.y = Math.max(0 + this.radius, Math.min(this.y + this.yVel, 666 - this.radius));
+    }
+}
 
 class Alien extends PIXI.Sprite{
     constructor(x, y, width, height, texture, speed, reward, radius){
@@ -46,7 +70,7 @@ class Alien extends PIXI.Sprite{
         this.yVel = 0;
     }
 
-    //tracks hero
+    //swarms hero
     hone(a, b) {
         let dx = a - this.x;
         let dy = b - this.y;
@@ -56,20 +80,28 @@ class Alien extends PIXI.Sprite{
         this.yVel = this.speed * Math.sin(honeAngle);
     }
 
-    //not used yet
-    collisionCircle(i, j){
+    
+    //keeps alien within canvas bounds
+    stayWithinBounds(a, b) {
+        this.hone(a, b);
+      
+        this.x = Math.max(0 + this.radius, Math.min(this.x + this.xVel, 1000 - this.radius));
+        this.y = Math.max(0 + this.radius, Math.min(this.y + this.yVel, 666 - this.radius));
+    }
+}
+
+   const collisionCircle = (i, j) => {
         let vx = i.x - j.x;
         let vy = i.y - j.y;
 
-        let mag = vx * vx + vy *vy;
+        let mag = vx * vx + vy * vy;
 
         let totalRad = i.radius + j.radius;
 
         return mag < (totalRad * totalRad);
     }
 
-    //not used yet
-    noOverlap(i, j) {
+    const noOverlap = (i, j) => {
         let vx = i.x - j.x;
         let vy = i.y - j.y;
 
@@ -77,28 +109,19 @@ class Alien extends PIXI.Sprite{
 
         let totalRad = i.radius + j.radius;
 
-        if(this.collisionCircle(i, j)){
+        if(collisionCircle(i, j)){
             let overlap = totalRad - mag;
 
-            dx = vx / mag;
-            dy = vy / mag;
+            let dx = vx / mag;
+            let dy = vy / mag;
 
             i.x += overlap * dx;
             i.y += overlap * dy;
         }
     }
     
-    //keeps alien within canvas bounds
-    stayWithinBounds(a, b) {
-        this.hone(a, b);
-        //don't know if WINDOW_WIDTH/WINDOW_HEIGHT is the correct variable
-        this.x = Math.max(0 + this.radius, Math.min(this.x + this.xVel, 1000 - this.radius));
-        this.y = Math.max(0 + this.radius, Math.min(this.y + this.yVel, 666 - this.radius));
-    }
-}
 
-
-let gameLoop = (delta) => {
+let gameLoop = () => {
 
     keyTracker.innerHTML = JSON.stringify(keys);
 
@@ -120,14 +143,26 @@ let gameLoop = (delta) => {
         }
 }
 
-let animate = () => {
+const animate = () => {
     hero.stayWithinArea();
+
+    gate.stayWithinArea();
+
     alien1.stayWithinBounds(hero.x, hero.y);
     alien2.stayWithinBounds(hero.x, hero.y);
     alien3.stayWithinBounds(hero.x, hero.y);
     alien4.stayWithinBounds(hero.x, hero.y);
     alien5.stayWithinBounds(hero.x, hero.y);
     alien6.stayWithinBounds(hero.x, hero.y);
+
+    /* for(let a = 0; a < aliens.length; a++){
+        for(let b = 0; b < aliens.length; b++){
+            noOverlap(aliens[a], aliens[b]);
+        }
+    } */
+
+    //requestAnimationFrame(animate);
+
 }
 
  //keyboard functions
@@ -153,6 +188,7 @@ let loadingFinished = () => {
         });
 
     container.addChild(hero);
+    container.addChild(gate);
     container.addChild(alien1);
     container.addChild(alien2);
     container.addChild(alien3);
@@ -168,14 +204,32 @@ let loadingFinished = () => {
     window.addEventListener("keydown", keyPressed);
     window.addEventListener("keyup", keyReleased);
 
+
     app.ticker.add(gameLoop);
-    app.ticker.add(animate);
+    app.ticker.add((delta) => {
+        gate.rotation += 0.005 * delta;
+
+        hero.stayWithinArea();
+
+        gate.stayWithinArea();
+
+        alien1.stayWithinBounds(hero.x, hero.y);
+        alien2.stayWithinBounds(hero.x, hero.y);
+        alien3.stayWithinBounds(hero.x, hero.y);
+        alien4.stayWithinBounds(hero.x, hero.y);
+        alien5.stayWithinBounds(hero.x, hero.y);
+        alien6.stayWithinBounds(hero.x, hero.y);
+
+    });
 
     keyTracker = document.querySelector("#keys");
+    
 }
 
-let initialSpawn = () => {
+const initialSpawn = () => {
     hero = new Human(app.view.width/2, app.view.height/2, 30, 30, app.loader.resources["hero"].texture, 10, 1);
+    gate = new Gate(250, 250, 90, 90, app.loader.resources["gate"].texture, 1, 25, 2);
+    gates.push(gate);
     alien1 = new Alien(10, 10, 30, 30, app.loader.resources["enemy"].texture, 4, 100, 2);
     aliens.push(alien1);
     alien2 = new Alien(20, 10, 30, 30, app.loader.resources["enemy"].texture, 4, 100, 2);
@@ -188,6 +242,10 @@ let initialSpawn = () => {
     aliens.push(alien5);
     alien6 = new Alien(30, 40, 30, 30, app.loader.resources["enemy"].texture, 4, 100, 2);
     aliens.push(alien6);
+}
+
+const gateSpawn = () => {
+    return new Gate((Math.random(Math.floor * 1000)), (Math.random(Math.floor * 666)), 90, 90, app.loader.resources["gate"].texture, 1, 25, 2);
 }
 
 
@@ -204,7 +262,8 @@ window.onload = () => {
     app.loader.baseUrl = "images";
     app.loader
         .add("hero", "hero.png")
-        .add("enemy", "enemy.png");
+        .add("enemy", "enemy.png")
+        .add("gate", "cleargate.png");
 
     app.loader.onError.add(errorReport);
     app.loader.onComplete.add(loadingFinished);
